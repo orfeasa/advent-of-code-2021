5c1b7d39521511d94a7af36950b37bf1f50b6190
Orfeas Antoniou <orfeas@orfeasa.com>
2021-11-18 19:54:53 +0000

    Fix script and python template file

--- day_xx/python/main.py
+++ day_xx/python/main.py
@@ -6,8 +6,8 @@ def part_two(filename: str) -> int:
     return 0
 
 
-if __name__ == '__main__':
-	input_path = "./day_xx/input.txt"
+if __name__ == "__main__":
+    input_path = "./day_xx/input.txt"
     print("---Part One---")
     print(part_one(input_path))
 
--- run_all.sh
+++ run_all.sh
@@ -4,15 +4,19 @@ for i in $(seq -f "%02g" 1 25)
 do
     GOFILE="./day_$i/go/main.go"
     PYFILE="./day_$i/python/main.py"
-    if [test -f "$GOFILE";] || [test -f "$PYFILE";] then
+    if test -f "$GOFILE"  || test -f "$PYFILE"
+    then
         echo "#### Day $i ####"
-        if [test -f "$GOFILE";] then
+        if test -f "$GOFILE"
+        then
             echo "Go: "
-            go run $FILE
+            go run $GOFILE
         fi
-        if [test -f "$PYFILE";] then
+
+        if test -f "$PYFILE";
+        then
             echo "Python: "
-            python3 $FILE
+            python3 $PYFILE
         fi
         printf "\n"
     fi
