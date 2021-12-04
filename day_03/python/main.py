a9073cd31c906bbc8907180ad4f2c20c0132efe9
Orfeas Antoniou <orfeas@orfeasa.com>
2021-12-04 12:40:30 +0000

    Dirty failed attempt for day 3 part 2

--- day_03/python/main.py
+++ day_03/python/main.py
@@ -10,7 +10,49 @@ def part_one(filename: str) -> int:
 
 
 def part_two(filename: str) -> int:
-    return 0
+    with open(filename) as f:
+        nums = list(map(lambda line: [int(i) for i in line.strip()], f.readlines()))
+
+    counters = [sum(col) for col in zip(*nums)]
+    gamma = "".join([str(int(c > len(nums) / 2)) for c in counters])
+    epsilon = "".join([str(int(c < len(nums) / 2)) for c in counters])
+
+    with open(filename) as f:
+        nums = list(map(lambda line: line.strip(), f.readlines()))
+
+    for suffix in range(1, len(nums[0])):
+        prefix = gamma[:-suffix] + "1"
+        matches = [x for x in nums if x.startswith(prefix)]
+        if len(matches):
+            oxygen = int(matches[0], 2)
+            if len(matches) > 1:
+                raise ValueError
+            break
+        prefix = gamma[:-suffix] + "0"
+        matches = [x for x in nums if x.startswith(prefix)]
+        if len(matches) > 0:
+            oxygen = int(matches[0], 2)
+            if len(matches) > 1:
+                raise ValueError
+            break
+
+    for suffix in range(1, len(nums[0])):
+        prefix = epsilon[:-suffix] + "1"
+        matches = [x for x in nums if x.startswith(prefix)]
+        if len(matches):
+            co2 = int(matches[0], 2)
+            if len(matches) > 1:
+                raise ValueError
+            break
+        prefix = epsilon[:-suffix] + "0"
+        matches = [x for x in nums if x.startswith(prefix)]
+        if len(matches) > 0:
+            co2 = int(matches[0], 2)
+            if len(matches) > 1:
+                raise ValueError
+            break
+
+    return oxygen * co2
 
 
 if __name__ == "__main__":
