9159c1ffe9c98a9f405014199445e0d3ff43c805
Orfeas Antoniou <orfeas@orfeasa.com>
2021-12-07 09:29:37 +0000

    Dirty attempt for day 5 part 2

--- day_05/python/main.py
+++ day_05/python/main.py
@@ -44,6 +44,25 @@ def part_two(filename: str) -> int:
     for pairs in lines:
         start = pairs[0]
         end = pairs[1]
+
+        # calculate step
+        if start[0] == end[0]:
+            x_step = 0
+        elif start[0] < end[0]:
+            x_step = 1
+        else:
+            x_step = -1
+
+        if start[1] == end[1]:
+            y_step = 0
+        elif start[1] < end[1]:
+            y_step = 1
+        else:
+            y_step = -1
+
+        step = (x_step, y_step)
+        dist = max(end[0] - start[0], end[1] - start[1])
+
         x_min = min(start[0], end[0])
         x_max = max(start[0], end[0])
         y_min = min(start[1], end[1])
@@ -53,8 +72,11 @@ def part_two(filename: str) -> int:
                 for y in range(y_min, y_max + 1):
                     pairs_to_lines[(x, y)] += 1
         else:
-            for step in range(0, x_max - x_min + 1):
-                pairs_to_lines[(x + step, y + step)] += 1
+            for i in range(dist + 1):
+                x = start[0] + i * step[0]
+                y = start[1] + i * step[1]
+                pairs_to_lines[(x, y)] += 1
+
     counter = 0
     for pairs, lines in pairs_to_lines.items():
         if lines >= 2:
