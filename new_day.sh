1056e3ad6a744b4d4877d66aa4bc04a67cb12547
Orfeas Antoniou <orfeas@orfeasa.com>
2021-12-10 11:37:38 +0000

    Rearrange new day script

--- new_day.sh
+++ new_day.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-YEAR=$(date +%Y)
-DAY=$(date +%-d)
-INPUT_URL="https://adventofcode.com/$YEAR/day/$DAY/input"
+COOKIE_FILE="cookie.txt"
 
+# create new directory for the day and replace input path in file
 DAY_PADDED=$(date +%d)
 DIR="day_$DAY_PADDED"
-TEMP_INPUT="temp-input.txt"
-COOKIE="`cat cookie.txt | tr -d '\n'`"
-
 cp -r "day_xx" "$DIR"
 sed -i '' "s/day_xx/$DIR/g" $DIR/main.py
 
-curl "https://adventofcode.com/2021/day/$DAY/input" --compressed \
+# fetch in put and save to input.txt
+COOKIE="`cat "$COOKIE_FILE" | tr -d '\n'`"
+YEAR=$(date +%Y)
+DAY=$(date +%-d)
+INPUT_URL="https://adventofcode.com/$YEAR/day/$DAY/input"
+TEMP_INPUT="temp-input.txt"
+curl "${INPUT_URL}" --compressed \
   -H "Cookie: session=${COOKIE}" \
   -o "${TEMP_INPUT}"
 
