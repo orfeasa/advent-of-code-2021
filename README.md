9db53e4caeff0539136941f95ceb3e7c30fbaaf9
Orfeas Antoniou <orfeas@orfeasa.com>
2021-12-13 15:42:57 +0000

    Solve day 13

--- README.md
+++ README.md
@@ -39,7 +39,7 @@ Make sure you have given permission to execute (`chmod +x run_all.sh`).
 | [10](https://adventofcode.com/2021/day/10) | Syntax Scoring          | ⭐⭐    |
 | [11](https://adventofcode.com/2021/day/11) | Dumbo Octopus           |       |
 | [12](https://adventofcode.com/2021/day/12) | Passage Pathing         |       |
-| [13](https://adventofcode.com/2021/day/13) | Transparent Origami     |       |
+| [13](https://adventofcode.com/2021/day/13) | Transparent Origami     | ⭐⭐  |
 | [14](https://adventofcode.com/2021/day/14) | TBA                     |       |
 | [15](https://adventofcode.com/2021/day/15) | TBA                     |       |
 | [16](https://adventofcode.com/2021/day/16) | TBA                     |       |
--- day_13/main.py
+++ day_13/main.py
@@ -1,50 +1,94 @@
+from collections import defaultdict
+
+
 def part_one(filename: str) -> int:
-    with open(filename) as f:
-        dots, folds = f.read().split("\n\n")
-        dots = list(
-            map(lambda line: tuple(map(int, line.strip().split(","))), dots.split("\n"))
-        )
-        folds = list(
-            map(
-                lambda line: (
-                    line.lstrip("fold along ").split("=")[0],
-                    int(line.lstrip("fold along ").split("=")[1]),
-                ),
-                folds.strip().split("\n"),
-            )
-        )
+    paper, x_max, y_max, folds = parse_input(filename)
 
-    paper = {(x, y): True for x, y, in dots}
+    for direction, val in folds:
+        paper, x_max, y_max = fold_paper(direction, val, paper, x_max, y_max)
+        break  # just one fold
 
-    x_max = max(map(lambda coord: coord[0], dots))
-    y_max = max(map(lambda coord: coord[1], dots))
-
-    direction, val = folds[0]
-    if direction == "x":
-        # folding along x0 means that the new grid becomes
-        # (x, y) = ((x, y) or (2*x0-x, y)) for x in [0, x0-1] and y in [0, y_max]
-        new_paper = {
-            (x, y): paper[(x, y)] or paper[(2 * val - x, x)]
-            for y in range(y_max)
-            for x in range(val)
-        }
-        x_max = val - 1
-        paper = new_paper
-
-    # folding along y0 means that the new grid becomes
-    # (x, y) = ((x, y) or (x, 2*y0-y)) for x in [0, x_max] and y in [0, y0-1]
-
-    return 0
+    return sum([int(val) for val in paper.values()])
 
 
 def part_two(filename: str) -> int:
-    return 0
+    paper, x_max, y_max, folds = parse_input(filename)
+
+    for direction, val in folds:
+        paper, x_max, y_max = fold_paper(direction, val, paper, x_max, y_max)
+
+    print_paper(paper, x_max, y_max)
+
+
+def fold_paper(direction, val, paper, x_max, y_max):
+    if direction == "x":
+        # folding along x0 means that the new grid becomes
+        # (x, y) = ((x, y) or (2*x0-x, y)) for x in [0, x0-1] and y in [0, y_max]
+        new_paper = defaultdict(bool)
+        new_paper.update(
+            {
+                (x, y): paper[(x, y)] or paper[(2 * val - x, y)]
+                for y in range(y_max + 1)
+                for x in range(val)
+            }
+        )
+        x_max = val - 1
+    elif direction == "y":
+        # folding along y0 means that the new grid becomes
+        # (x, y) = ((x, y) or (x, 2*y0-y)) for x in [0, x_max] and y in [0, y0-1]
+        new_paper = defaultdict(bool)
+        new_paper.update(
+            {
+                (x, y): paper[(x, y)] or paper[(x, 2 * val - y)]
+                for y in range(val)
+                for x in range(x_max + 1)
+            }
+        )
+        y_max = val - 1
+    paper = new_paper
+    return paper, x_max, y_max
+
+
+def parse_input(filename: str):
+    with open(filename) as f:
+        dots, folds = f.read().split("\n\n")
+
+    dots = list(
+        map(lambda line: tuple(map(int, line.strip().split(","))), dots.split("\n"))
+    )
+    folds = list(
+        map(
+            lambda line: (
+                line.lstrip("fold along ").split("=")[0],
+                int(line.lstrip("fold along ").split("=")[1]),
+            ),
+            folds.strip().split("\n"),
+        )
+    )
+    paper = defaultdict(bool)
+    paper.update({(x, y): True for x, y, in dots})
+
+    x_max = max(map(lambda coord: coord[0], dots))
+    y_max = max(map(lambda coord: coord[1], dots))
+    return paper, x_max, y_max, folds
+
+
+def print_paper(paper: dict[tuple[int, int], bool], x_max, y_max: int):
+    for y in range(y_max + 1):
+        line_str = ""
+        for x in range(x_max + 1):
+            if paper[(x, y)]:
+                line_str += "#"
+            else:
+                line_str += "."
+        print(line_str)
+    print("")
 
 
 if __name__ == "__main__":
-    input_path = "./day_13/test_input.txt"
+    input_path = "./day_13/input.txt"
     print("---Part One---")
     print(part_one(input_path))
 
     print("---Part Two---")
-    print(part_two(input_path))
+    part_two(input_path)
