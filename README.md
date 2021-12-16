55b3666c75f7b935643d970396553e20736f075a
Orfeas Antoniou <orfeas@orfeasa.com>
2021-12-16 16:08:46 +0000

    Solve day 11

--- README.md
+++ README.md
@@ -37,7 +37,7 @@ Make sure you have given permission to execute (`chmod +x run_all.sh`).
 | [08](https://adventofcode.com/2021/day/8)  | Seven Segment Search    | ⭐⭐    |
 | [09](https://adventofcode.com/2021/day/9)  | Smoke Basin             | ⭐⭐    |
 | [10](https://adventofcode.com/2021/day/10) | Syntax Scoring          | ⭐⭐    |
-| [11](https://adventofcode.com/2021/day/11) | Dumbo Octopus           |       |
+| [11](https://adventofcode.com/2021/day/11) | Dumbo Octopus           | ⭐⭐    |
 | [12](https://adventofcode.com/2021/day/12) | Passage Pathing         |       |
 | [13](https://adventofcode.com/2021/day/13) | Transparent Origami     | ⭐⭐    |
 | [14](https://adventofcode.com/2021/day/14) | Extended Polymerization | ⭐     |
--- day_11/main.py
+++ day_11/main.py
@@ -3,52 +3,57 @@ def part_one(filename: str) -> int:
         nums = list(map(lambda n: [int(x) for x in list(n.strip())], f.readlines()))
 
     count_flashes = 0
-    print("Before any steps:")
-    print_nums(nums)
-    for step in range(101 + 1):
-        for y, line in enumerate(nums):
-            for x, _ in enumerate(line):
-                nums[y][x] += 1
-
-        increment_queue = []
-        to_flash = [
-            (x, y)
-            for y, line in enumerate(nums)
-            for x, val in enumerate(line)
-            if val > 9
-        ]
-
-        flashed = set()
-        while to_flash:
-            x, y = to_flash.pop(0)
-            flashed.add((x, y))
-            increment_queue = get_adjacents(x, y, nums)
-            for x, y in increment_queue:
-                nums[y][x] += 1
-                if nums[y][x] > 9 and (x, y) not in flashed:
-                    to_flash.append((x, y))
-
-        for y, line in enumerate(nums):
-            for x, _ in enumerate(line):
-                if nums[y][x] > 9:
-                    nums[y][x] = 0
-
-        count_flashes += len(flashed)
-        print(f"After step {step+1}")
-        print_nums(nums)
-        print(f"Flashed: {len(flashed)}: {flashed}")
+    for _ in range(100):
+        nums = next_step(nums)
+        count_flashes += sum([int(num == 0) for line in nums for num in line])
 
     return count_flashes
 
 
 def part_two(filename: str) -> int:
-    return 0
+    with open(filename) as f:
+        nums = list(map(lambda n: [int(x) for x in list(n.strip())], f.readlines()))
+
+    synced = False
+    step = 0
+    while not synced:
+        step += 1
+        nums = next_step(nums)
+        synced = all([num == nums[0][0] for line in nums for num in line])
+    return step
+
+
+def next_step(nums):
+    for y, line in enumerate(nums):
+        for x, _ in enumerate(line):
+            nums[y][x] += 1
+
+    to_flash = {
+        (x, y) for y, line in enumerate(nums) for x, val in enumerate(line) if val > 9
+    }
+
+    flashed = set()
+
+    while to_flash:
+        x0, y0 = to_flash.pop()
+        flashed.add((x0, y0))
+        for x, y in get_adjacents(x0, y0, nums):
+            if (x, y) not in flashed | to_flash:
+                nums[y][x] += 1
+                if nums[y][x] > 9:
+                    to_flash.add((x, y))
+
+    for y, line in enumerate(nums):
+        for x, _ in enumerate(line):
+            if nums[y][x] > 9:
+                nums[y][x] = 0
+    return nums
 
 
 def get_adjacents(x, y: int, nums: list[list[int]]) -> list[tuple[int, int]]:
     return [
-        (y + y1, x + x1)
-        for x1, y1 in [
+        (x + dx, y + dy)
+        for dx, dy in [
             (-1, -1),
             (-1, 0),
             (-1, 1),
@@ -58,17 +63,8 @@ def get_adjacents(x, y: int, nums: list[list[int]]) -> list[tuple[int, int]]:
             (1, 0),
             (1, 1),
         ]
-        if 0 <= x + x1 < len(nums[0]) and 0 <= y + y1 < len(nums)
+        if 0 <= x + dx < len(nums[0]) and 0 <= y + dy < len(nums)
     ]
-
-
-def print_nums(nums: list[list[int]]):
-    for line in nums:
-        line_str = ""
-        for ch in line:
-            line_str += str(ch)
-        print(line_str)
-    print("")
 
 
 if __name__ == "__main__":
